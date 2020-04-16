2248eef5efa854a284d7acc61a0ffa259f2055d5
Kevin <kevinpanacci@hotmail.it>
2020-04-16 18:21:58 +0200

    Complete

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
+<!-- Stampare una stringa verde se la variabile password passata in GET è uguale a "Boolean", altrimenti stampare una stringa rossa -->
+
+
 <?php
-
-
-
-
+    $mypass = $_GET['mypass'];
+    $password = 'Boolean';
+    if ($mypass == $password) {
+        echo '<p style="color: green">Accesso consentito!</p>';
+    } else {
+        echo '<p style="color: red">La password non è corretta</p>';
+    }
 ?>
